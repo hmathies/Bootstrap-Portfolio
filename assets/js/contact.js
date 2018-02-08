163cb3ab5df03ac5cff45acc5206023f00516d59
Heather <hlmathies@gmail.com>
2018-02-08 15:38:08 -0500

    test clear form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,26 +1,43 @@
 
-
 $( document ).ready(function() {
 
-    $('form').submit(function(event) {
-        event.preventDefault();
-      
+       $('form').submit(function(event) {
+           event.preventDefault();
 
-        $.ajax({
-            url: "https://formspree.io/hlmathies@gmail.com",
-            method: "POST",
-            data: {
-                name: $('#js-name').val(),
-                email: $('#js-email').val(),
-                message: $('#js-message').val()
-            },
-            dataType: "json"
-        }).error(function(jqXHR, status, error){
-            alert('Ooops! Something went wrong. Please try again."');
-        }).success(function(data, status, jqXHR) {
-            alert('Thank you for your message!');
-            $('form').find('#js-name, #js-email, #js-message').val('');
-        });
-    });
+           $.ajax({
+               url: "https://formspree.io/hlmathies@gmail.com",
+               method: "POST",
+               dataType: "json",
+               data: {
+                   name: $('#js-name').val(),
+                   email: $('#js-email').val(),
+                   message: $('#js-message').val()
+               },
+               success: function(data){
+                   $("form").trigger("reset"); //reset form
 
-});
+               }
+       });
+
+   });
+
+ });
+
+
+        //
+        //    $.ajax({
+        //        url: 'insert.php',
+        //        dataType: 'text',
+        //        type: 'post',
+        //        contentType: 'application/x-www-form-urlencoded',
+        //        data: $(this).serialize(),
+        //        success: function( data, textStatus, jQxhr ){
+        //         $('#alertt').fadeIn(2000);
+        //         $('#alertt').html( data );
+        //         $('#alertt').fadeOut(3000);
+        //        resetForm('userInf');
+        //     },
+        //     error: function( jqXhr, textStatus, errorThrown ){
+        //         console.log( errorThrown );
+        //     }
+        // });
