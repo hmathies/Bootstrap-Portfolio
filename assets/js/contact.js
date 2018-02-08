e118c491f5314d4a4be51ebfcb4ae2ded9519808
Heather <hlmathies@gmail.com>
2018-02-08 13:27:19 -0500

    Fixing ajax call

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,49 +1,50 @@
-$(document).ready(function(){
-
-
-    $(".submit").click(function(event){
-      event.preventDefault();
-
-      $.ajax({
-          url: "http://formspree.io/hlmathies@gmail.com",
-          method: "POST",
-          data: {
-              name: $('#js-name').val(),
-              email: $('#js-email').val(),
-              message: $('#js-message').val()
-          },
-          dataType: "json"
-        }).error(function(jqXHR, status, error){
-          alert("Ooops! Something went wrong. Please try again.");
-        }).success(function(data,status,jqXHR){
-          alert("Thanks for your message!");
-          $(".submit").find("#js-name, #js-email, #js-message").val("");
-        })
-      });
-    });
-
-});
-
-
-// $( document ).ready(function() {
+// $(document).ready(function(){
 //
-//     $('form').submit(function(event) {
-//         event.preventDefault();
 //
-//         $.ajax({
-//             url: "http://formspree.io/alena@makeschool.com",
-//             method: "POST",
-//             data: {
-//                 email: $('#js-email').val(),
-//                 message: $('#js-message').val()
-//             },
-//             dataType: "json"
+//     $("form").submit(function(event){
+//       event.preventDefault();
+//
+//       $.ajax({
+//           url: "http://formspree.io/hlmathies@gmail.com",
+//           method: "POST",
+//           data: {
+//               name: $('#js-name').val(),
+//               email: $('#js-email').val(),
+//               message: $('#js-message').val()
+//           },
+//           dataType: "json"
 //         }).error(function(jqXHR, status, error){
-//             alert('Uh oh, something went wrong. Please try again.');
-//         }). success(function(data, status, jqXHR) {
-//             alert('Thank you for your message. Kitty will get back to you soon.');
-//             $('form').find('#js-email, #js-message').val('');
-//         });
+//           alert("Ooops! Something went wrong. Please try again.");
+//         }).success(function(data,status,jqXHR){
+//           alert("Thanks for your message!");
+//           $("form").find("#js-name, #js-email, #js-message").val("");
+//
+//       });
 //     });
 //
 // });
+
+
+$( document ).ready(function() {
+
+    $('form').submit(function(event) {
+        event.preventDefault();
+
+        $.ajax({
+            url: "http://formspree.io/hlmathies@gmail.com",
+            method: "POST",
+            data: {
+                name: $('#js-name').val(),
+                email: $('#js-email').val(),
+                message: $('#js-message').val()
+            },
+            dataType: "json"
+        }).error(function(jqXHR, status, error){
+            alert('Uh oh, something went wrong. Please try again.');
+        }).success(function(data, status, jqXHR) {
+            alert('Thank you for your message. Kitty will get back to you soon.');
+            $('form').find('#js-name, #js-email, #js-message').val('');
+        });
+    });
+
+});
