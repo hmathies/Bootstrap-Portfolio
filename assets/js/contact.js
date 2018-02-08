e67ad7e5d14dccaa672f73ee6431afdd7c33cf35
Heather <hlmathies@gmail.com>
2018-02-08 13:17:58 -0500

    Add ajax call to contact form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,8 +1,49 @@
 $(document).ready(function(){
 
 
-$("#submit").submit(function(event){
-  alert("form submitted!")
-});
+    $(".submit").click(function(event){
+      event.preventDefault();
+
+      $.ajax({
+          url: "http://formspree.io/hlmathies@gmail.com",
+          method: "POST",
+          data: {
+              name: $('#js-name').val(),
+              email: $('#js-email').val(),
+              message: $('#js-message').val()
+          },
+          dataType: "json"
+        }).error(function(jqXHR, status, error){
+          alert("Ooops! Something went wrong. Please try again.");
+        }).success(function(data,status,jqXHR){
+          alert("Thanks for your message!");
+          $(".submit").find("#js-name, #js-email, #js-message").val("");
+        })
+      });
+    });
 
 });
+
+
+// $( document ).ready(function() {
+//
+//     $('form').submit(function(event) {
+//         event.preventDefault();
+//
+//         $.ajax({
+//             url: "http://formspree.io/alena@makeschool.com",
+//             method: "POST",
+//             data: {
+//                 email: $('#js-email').val(),
+//                 message: $('#js-message').val()
+//             },
+//             dataType: "json"
+//         }).error(function(jqXHR, status, error){
+//             alert('Uh oh, something went wrong. Please try again.');
+//         }). success(function(data, status, jqXHR) {
+//             alert('Thank you for your message. Kitty will get back to you soon.');
+//             $('form').find('#js-email, #js-message').val('');
+//         });
+//     });
+//
+// });
